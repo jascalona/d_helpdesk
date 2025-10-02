70d55207cb6fba7969ffd0a2b0f0d1698684de81
unknown <abrahamjose.1587@gmail.com>
2025-10-02 11:28:51 -0400

    se realizo la primera integracion con API, consumo de data desde table_i

--- src/resources/component/table.tsx
+++ src/resources/component/table.tsx
@@ -1,373 +1,59 @@
-import * as React from 'react';
-import { alpha } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import DeleteIcon from '@mui/icons-material/Delete';
-import FilterListIcon from '@mui/icons-material/FilterList';
-import { visuallyHidden } from '@mui/utils';
-// 👇 Importa el componente TextField de Material-UI para la barra de búsqueda
-import TextField from '@mui/material/TextField';
-// 👇 Importa el ícono de búsqueda
-import SearchIcon from '@mui/icons-material/Search';
+import { useState } from "react";
+import { DataTable } from 'primereact/datatable';
+import { useEffect } from "react";
+import { Column } from 'primereact/column';
+import ViewOrganization from './view_organizacion';
+import axios from 'axios';
 
-// Definición de tipos genéricos para que el componente sea flexible
-type Order = 'asc' | 'desc';
-
-function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
+interface Empresa {
+    co_emp: string;
+    nb_emp: string;
+    st_estado: string;
+    fe_registro: string;
+    auto: string;
 }
 
-function getComparator<T>(
-    order: Order,
-    orderBy: keyof T,
-): (
-    a: T,
-    b: T,
-) => number {
-    return order === 'desc'
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
+function Table_I() {
+    const [empresas, setEmpresas] = useState<Empresa[]>([]);
+    const [cargando, setCargando] = useState(true);
 
-// Interfaz para las celdas de la cabecera, ahora genérica
-interface HeadCell<T> {
-    disablePadding: boolean;
-    id: keyof T;
-    label: string;
-    numeric: boolean;
-}
+    // Definicion de la configuracion de las cabeceras
 
-// Interfaz para las props del componente de la cabecera, también genérica
-interface EnhancedTableHeadProps<T extends { id: number }> {
-    numSelected: number;
-    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof T) => void;
-    onSelectAllClick: (event: React.ChangeEvent<HTMLInputElement>) => void;
-    order: Order;
-    orderBy: keyof T;
-    rowCount: number;
-    headCells: readonly HeadCell<T>[];
-}
-
-function EnhancedTableHead<T extends { id: number }>(props: EnhancedTableHeadProps<T>) {
-    const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort, headCells } = props;
-    const createSortHandler =
-        (property: keyof T) => (event: React.MouseEvent<unknown>) => {
-            onRequestSort(event, property);
-        };
-
-    return (
-        <TableHead>
-            <TableRow>
-                <TableCell padding="checkbox">
-                    <Checkbox
-                        color="primary"
-                        indeterminate={numSelected > 0 && numSelected < rowCount}
-                        checked={rowCount > 0 && numSelected === rowCount}
-                        onChange={onSelectAllClick}
-                        inputProps={{
-                            'aria-label': 'select all items',
-                        }}
-                    />
-                </TableCell>
-                {headCells.map((headCell) => (
-                    <TableCell
-                        key={headCell.id as string}
-                        align={headCell.numeric ? 'right' : 'left'}
-                        padding={headCell.disablePadding ? 'none' : 'normal'}
-                        sortDirection={orderBy === headCell.id ? order : false}
-                    >
-                        <TableSortLabel
-                            active={orderBy === headCell.id}
-                            direction={orderBy === headCell.id ? order : 'asc'}
-                            onClick={createSortHandler(headCell.id)}
-                        >
-                            {headCell.label}
-                            {orderBy === headCell.id ? (
-                                <Box component="span" sx={visuallyHidden}>
-                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                                </Box>
-                            ) : null}
-                        </TableSortLabel>
-                    </TableCell>
-                ))}
-            </TableRow>
-        </TableHead>
-    );
-}
-
-interface EnhancedTableToolbarProps {
-    numSelected: number;
-    // 👇 Añade una prop para el texto de búsqueda**
-    filterText: string;
-    // 👇 Añade una prop para la función de búsqueda**
-    onFilterChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
-function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
-    const { numSelected, filterText, onFilterChange } = props;
-    return (
-        <Toolbar
-            sx={[
-                {
-                    pl: { sm: 2 },
-                    pr: { xs: 1, sm: 1 },
-                    background: '#5a639c26'
-                }
-                ,
-                numSelected > 0 && {
-                    bgcolor: (theme) =>
-                        alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-                },
-            ]}
-        >
-            {numSelected > 0 ? (
-                <Typography
-                    sx={{ flex: '1 1 100%' }}
-                    color="inherit"
-                    variant="subtitle1"
-                    component="div"
-                >
-                    {numSelected} selected
-                </Typography>
-            ) : (
-                <Box sx={{ flex: '1 1 100%', display: 'flex', alignItems: 'center' }}>
-                    <TextField
-                        label="Buscar..."
-                        variant="standard"
-                        value={filterText}
-                        onChange={onFilterChange}
-                        size="small"
-                        sx={{ flex: 1, }}
-                    />
-                </Box>
-            )}
-            {numSelected > 0 ? (
-                <Tooltip title="Delete">
-                    <IconButton>
-                        <DeleteIcon />
-                    </IconButton>
-                </Tooltip>
-            ) : (
-                <Tooltip title="Filter list">
-                    <IconButton>
-                        <FilterListIcon />
-                    </IconButton>
-                </Tooltip>
-            )}
-        </Toolbar>
-    );
-}
-
-// Interfaz para las props del componente principal, ahora genérica
-interface EnhancedTableProps<T extends { id: number }> {
-    data: T[];
-    headCells: readonly HeadCell<T>[];
-    title: string;
-}
-
-export default function EnhancedTable<T extends { id: number }>(props: EnhancedTableProps<T>) {
-    const { data: rows, headCells, title } = props;
-    const [order, setOrder] = React.useState<Order>('asc');
-    const [orderBy, setOrderBy] = React.useState<keyof T>('id' as keyof T);
-    const [selected, setSelected] = React.useState<readonly number[]>([]);
-    const [page, setPage] = React.useState(0);
-    const [dense, setDense] = React.useState(false);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
-    // 👇 Nuevo estado para el texto de búsqueda**
-    const [filterText, setFilterText] = React.useState('');
-
-    const handleRequestSort = (
-        event: React.MouseEvent<unknown>,
-        property: keyof T,
-    ) => {
-        const isAsc = orderBy === property && order === 'asc';
-        setOrder(isAsc ? 'desc' : 'asc');
-        setOrderBy(property);
-    };
-
-    const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-        if (event.target.checked) {
-            const newSelected = rows.map((n) => n.id);
-            setSelected(newSelected);
-            return;
-        }
-        setSelected([]);
-    };
-
-    const handleClick = (event: React.MouseEvent<unknown>, id: number) => {
-        const selectedIndex = selected.indexOf(id);
-        let newSelected: readonly number[] = [];
-
-        if (selectedIndex === -1) {
-            newSelected = newSelected.concat(selected, id);
-        } else if (selectedIndex === 0) {
-            newSelected = newSelected.concat(selected.slice(1));
-        } else if (selectedIndex === selected.length - 1) {
-            newSelected = newSelected.concat(selected.slice(0, -1));
-        } else if (selectedIndex > 0) {
-            newSelected = newSelected.concat(
-                selected.slice(0, selectedIndex),
-                selected.slice(selectedIndex + 1),
-            );
-        }
-        setSelected(newSelected);
-    };
-
-    const handleChangePage = (event: unknown, newPage: number) => {
-        setPage(newPage);
-    };
-
-    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
-
-    const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setDense(event.target.checked);
-    };
-
-    // 👇 Nuevo handler para la búsqueda**
-    const handleFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFilterText(event.target.value);
-        setPage(0); // Reinicia la paginación al filtrar**
-    };
-
-    // 👇 Filtra los datos antes de ordenarlos y paginarlos
-    const filteredRows = React.useMemo(() => {
-        if (filterText === '') {
-            return rows;
-        }
-        return rows.filter((row) =>
-            headCells.some((headCell) => {
-                const value = String(row[headCell.id]).toLowerCase();
-                return value.includes(filterText.toLowerCase());
+    useEffect(() => {
+        axios.get<Empresa[]>('http://localhost:8080/basetomee/empresas/listar')
+            .then(response => {
+                setEmpresas(response.data);
+                setCargando(false);
             })
-        );
-    }, [rows, filterText, headCells]);
+            .catch(error => {
+                console.error('Hubo un error al obtener los registros:', error);
+                setCargando(false);
+            });
+    }, []);
 
-    const emptyRows =
-        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - filteredRows.length) : 0;
-
-    const visibleRows = React.useMemo(
-        () =>
-            [...filteredRows]
-                .sort(getComparator(order, orderBy))
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage),
-        [order, orderBy, page, rowsPerPage, filteredRows],
-    );
-
-    const rowCount = filteredRows.length;
+    if (cargando) return <p>Cargando registros...</p>;
 
     return (
-        <Box sx={{ width: '100%',  }}>
-            <Paper sx={{ width: '100%', mb: 2 }}>
-                <EnhancedTableToolbar
-                    numSelected={selected.length}
-                    filterText={filterText}
-                    onFilterChange={handleFilterChange}
-                />
-                <TableContainer>
-                    <Table
-                        sx={{ minWidth: 750 }}
-                        aria-labelledby="tableTitle"
-                        size={dense ? 'small' : 'medium'}
-                    >
-                        <EnhancedTableHead
-                            numSelected={selected.length}
-                            order={order}
-                            orderBy={orderBy}
-                            onSelectAllClick={handleSelectAllClick}
-                            onRequestSort={handleRequestSort}
-                            rowCount={rowCount}
-                            headCells={headCells}
-                        />
-                        <TableBody >
-                            {visibleRows.map((row, index) => {
-                                const isItemSelected = selected.includes(row.id);
-                                const labelId = `enhanced-table-checkbox-${index}`;
+        <>
+            <div className="table">
+            
+                <DataTable
+                    value={empresas}
+                    tableStyle={{ minWidth: '50rem' }}
+                    paginator rows={10} // Opcional: añadir paginación
+                    emptyMessage="No hay empresas registradas"
+                >
 
-                                return (
-                                    <TableRow
-                                        hover
-                                        onClick={(event) => handleClick(event, row.id)}
-                                        role="checkbox"
-                                        aria-checked={isItemSelected}
-                                        tabIndex={-1}
-                                        key={row.id}
-                                        selected={isItemSelected}
-                                        sx={{ cursor: 'pointer' }}
-                                    >
-                                        <TableCell padding="checkbox">
-                                            <Checkbox
-                                                color="primary"
-                                                checked={isItemSelected}
-                                                inputProps={{
-                                                    'aria-labelledby': labelId,
-                                                }}
-                                            />
-                                        </TableCell>
-                                        {headCells.map((headCell, cellIndex) => (
-                                            <TableCell
-                                                key={`${row.id}-${headCell.id as string}`}
-                                                align={headCell.numeric ? 'right' : 'left'}
-                                                component={cellIndex === 0 ? 'th' : 'td'}
-                                                scope={cellIndex === 0 ? 'row' : undefined}
-                                                padding={headCell.disablePadding ? 'none' : 'normal'}
-                                            >
-                                                {/* Se renderiza el valor de la celda de forma dinámica */}
-                                                {row[headCell.id] as React.ReactNode}
-                                            </TableCell>
-                                        ))}
-                                    </TableRow>
-                                );
-                            })}
-                            {emptyRows > 0 && (
-                                <TableRow
-                                    style={{
-                                        height: (dense ? 33 : 53) * emptyRows,
-                                    }}
-                                >
-                                    <TableCell colSpan={headCells.length + 1} />
-                                </TableRow>
-                            )}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[5, 10, 25]}
-                    component="div"
-                    count={filteredRows.length}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                />
-            </Paper>
-            <FormControlLabel
-                control={<Switch checked={dense} onChange={handleChangeDense} />}
-                label="Dense padding"
-            />
-        </Box>
-    );
+                    <Column field="co_emp" header="RIF"></Column>
+                    <Column field="nb_emp" header="Nm Organizacion"></Column>
+                    <Column field="st_estado" header="Estado"></Column>
+                    <Column field="fe_registro" header="Fe Registro"></Column>
+                    <Column field="auto" header="Autor"></Column>
+
+                </DataTable>
+            </div>
+        </>
+    )
 }
+
+export default Table_I;
--- src/resources/component/view_organizacion.tsx
+++ src/resources/component/view_organizacion.tsx
@@ -1,39 +1,23 @@
 import { useState } from "react";
+import { useEffect } from "react";
 import EnhancedTable from './table';
-
-// 1. Define el tipo de dato para tus filas
-interface UserData {
-    id: number;
-    rif: string
-    name: string;
-    estado: string;
-    autor: string
-}
-
-// 2. Prepara los datos (este sería el "fetch" de tu API)
-const users: UserData[] = [
-    { id: 1, rif: "J1234567", name: 'John Doe', estado: 'ACTIVO', autor: "Jose Escalona" },
-    { id: 2, rif: "J1234568", name: 'Jane Smith', estado: 'ACTIVO', autor: "Jose Escalona" },
-    { id: 3, rif: "J1234569", name: 'Peter Jones', estado: 'ACTIVO', autor: "Jose Escalona" },
-];
-
-// 3. Define la configuración de las cabeceras
-const userHeadCells = [
-    { id: 'id' as keyof UserData, numeric: true, disablePadding: false, label: 'id' },
-    { id: 'rif' as keyof UserData, numeric: false, disablePadding: true, label: 'RIF' },
-    { id: 'name' as keyof UserData, numeric: false, disablePadding: true, label: 'Organizacion' },
-    { id: 'estado' as keyof UserData, numeric: false, disablePadding: false, label: 'Estado' },
-    { id: 'autor' as keyof UserData, numeric: false, disablePadding: true, label: 'autor' },
-];
-
+import axios from 'axios';
+import Table_i from './table';
+import '../../assets/CSS/view_organitation.css';
 
 function ViewOrganization() {
+
     return (
         <>
+            
+            <div className="section-header">
+                <h2>Diseño Organizacional</h2>
+            </div>
 
-            <EnhancedTable data={users} headCells={userHeadCells} title="Usuarios" />
+            <div className="container-table-detalles">
+                <Table_i />
+            </div>
         </>
     )
-
 }
 export default ViewOrganization
